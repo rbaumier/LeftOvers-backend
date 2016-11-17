b98baed02c4d9bc818247f022580d4ab98f52078
Romain Baumier <baumier.romain@gmail.com>
2016-11-17 19:54:11 +0100

    fix(AuthService.check)

--- src/api/services/AuthService.js
+++ src/api/services/AuthService.js
@@ -1,46 +1,32 @@
 'use strict';
 var JWT = require('jsonwebtoken');
 
-module.exports = ({dealers, users}, config) => {
-    return {
-        check(decoded, request, callback) {
-            var token = request.headers.authorization;
-
-            // do your checks to see if the token is valid
-            if (!decoded.email || !decoded.password || (Math.floor(Date.now() / 1000) > decoded.exp)) {
-                return callback(null, false);
-            } else {
-                users.findOneBy({token: token}, function (err, user) {
-                    if (err) {
-                        return f(Boom.wrap(err));
-                    }
-
-                    if (!user) {
-                        dealers.findOneBy({token: token}, function (err, dealer) {
-                            if (err) {
-                                return f(Boom.wrap(err));
-                            }
-
-                            if (!dealer) {
-                                return callback(null, false);
-                            } else {
-                                if (dealer.token === token) {
-                                    return callback(null, true);
-                                }
-                                return callback(null, true);
-                            }
-                        });
-                    } else {
-                        if (user.token === token) {
-                            return callback(null, true);
-                        }
-                    }
-                });
-            }
-        },
-
-        generateToken(credentials) {
-            return JWT.sign(credentials, config.api.jwt2.secret);
+module.exports = ({ dealers, users }, config) => {
+  return {
+    check(decoded, request, f) {
+      var token = request.headers.authorization.replace('Bearer ', ''); // remove Bearer text if present
+      // do your checks to see if the token is valid
+      if (!decoded.email || !decoded.password || (Math.floor(Date.now() / 1000) > decoded.exp)) {
+        return f(null, false);
+      }
+      users.findOneBy({ token }, (err, user) => {
+        if (err) {
+          return f(Boom.wrap(err));
         }
-    };
+        if (user) {
+          return f(null, user.token === token);
+        }
+        dealers.findOneBy({ token }, (err, dealer) => {
+          if (err) {
+            return f(Boom.wrap(err));
+          }
+          f(null, !dealer ? false : dealer.token === token);
+        });
+      });
+    },
+
+    generateToken(credentials) {
+      return JWT.sign(credentials, config.api.jwt2.secret);
+    }
+  };
 };
